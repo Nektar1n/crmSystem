e049910a8a6dd65c2e2394a35c580b2dd1ba2aa0
Nikita <vysokij96@bk.ru>
2022-04-26 12:52:47 +0300

    added chart Pie graph

--- pages/history/index.vue
+++ pages/history/index.vue
@@ -3,15 +3,15 @@
   <h1>
     История записей
   </h1>
-  <div class="history-chart">
-<!--    <canvas ref="canvas"></canvas>-->
-    <div class="container">
-      <bar-chart :data="barChartData" :options="barChartOptions" :height="200" />
-    </div>
-  </div>
+
   <Loader v-if="loading"/>
   <p class="center" v-else-if="!records.length">Записей пока нет</p>
   <section v-else>
+    <div class="history-chart">
+      <div class="container">
+        <pie-chart :data="barChartData" :options="barChartOptions" :height="200" />
+      </div>
+    </div>
     <history-table :records="records"/>
   </section>
 </v-container>
@@ -20,7 +20,7 @@
 <script>
 import HistoryTable from "../../components/HistoryTable";
 import Loader from "../../components/app/Loader";
-import BarChart from '~/components/chart'
+import PieChart from "../../components/PieChart";
 
 const chartColors = {
   red: 'rgb(255, 99, 132)',
@@ -35,26 +35,18 @@ const chartColors = {
 
 export default {
   name: "history",
-  components: { Loader, HistoryTable, BarChart},
+  components: { Loader, HistoryTable, PieChart},
   data: ()=>({
     loading: true,
     records:[],
     categories:[],
     barChartData: {
-      labels: ['Jan', 'Feb', 'Mar'],
-      datasets: [
-        {
-          label: 'Income',
-          // backgroundColor: ["red", "orange", "yellow"],
-          backgroundColor: [chartColors.red, chartColors.orange, chartColors.yellow],
-          data: [10, 15, 20]
-        }
-      ]
+
     },
     barChartOptions: {
       responsive: true,
       legend: {
-        display: false,
+        display: true,
       },
       title: {
         display: true,
@@ -90,7 +82,41 @@ export default {
     })
 
 
-    this.loading=false;
+    this.barChartData={
+      labels: this.categories.map(c=>c.title),
+      datasets: [{
+        label: 'Расходы по категориям',
+        data: this.categories.map(c=>{
+          return this.records.reduce((total, rec)=>{
+            if (rec.categoryId===c.id && rec.type==='outcome'){
+              total += +rec.sum
+            }
+            return total
+          }, 0)
+        }),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(54, 162, 235, 0.5)',
+          'rgba(255, 206, 86, 0.5)',
+          'rgba(75, 192, 192, 0.4)',
+          'rgba(153, 102, 255, 0.5)',
+          'rgba(255, 159, 64, 0.5)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    }
+
+
+    // setTimeout(()=>this.loading=false,400)
+    this.loading=false
   },
 
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,60 @@
 <template>
   <v-card dark>
   <h1>USER, HELLO!</h1>
-<!--    <h1>{{$store.state.auth.user}}</h1>-->
+    <div class="container">
+      <bar-chart :data="barChartData" :options="barChartOptions" :height="200" />
+    </div>
   </v-card>
 </template>
 
 <script>
+import BarChart from '~/components/chart'
+
+const chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(201, 203, 207)'
+};
 
 export default {
   name: 'IndexPage',
-
+  components:{BarChart},
+  data: ()=>({
+    barChartData: {
+      labels: ['Jan', 'Feb', 'Mar'],
+      datasets: [
+        {
+          label: 'Income',
+          // backgroundColor: ["red", "orange", "yellow"],
+          backgroundColor: [chartColors.red, chartColors.orange, chartColors.yellow],
+          data: [10, 15, 20]
+        }
+      ]
+    },
+    barChartOptions: {
+      responsive: true,
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: 'Monthly Income'
+      },
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    },
+  })
   // mounted() {
   //   console.log(this.$store.state.auth.user)
   // },
